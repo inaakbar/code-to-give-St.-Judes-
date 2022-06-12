86fc7f06f7ae09daada95cfde4cb1c8312271cfa
farhan-alam2000 <alaamm.farhan@gmail.com>
2022-06-12 16:00:22 +0530

    Login pages final

--- farhan/main.js
+++
@@ -1,9 +0,0 @@
-if('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        console.log('App is loader');
-        navigator.serviceWorker.register('sw_cached_pages.js')
-            .then( () => {
-                console.log("Service Worker registerd");
-            })
-    })
-}
